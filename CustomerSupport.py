f04b8be90ff4cc02a542c3445c34c27ba0b7af7d
RayhanJamasi <162825472+RayhanJamasi@users.noreply.github.com>
2025-05-24 21:17:04 -0400

    Customer Focused Systems
    CrewAI simple programs more focused on customer support and working with them

---
+++ CustomerSupport.py
@@ -0,0 +1,139 @@
+# Warning control
+import warnings
+warnings.filterwarnings('ignore')
+
+from crewai import Agent, Task, Crew
+
+import os
+from langchain_openai import ChatOpenAI
+
+#loading the environment variables from the .env file
+from dotenv import load_dotenv
+load_dotenv()
+
+#getting the API keys securely (compared to hardcoding which is not good for security)
+openai_api_key = os.getenv("OPENAI_API_KEY")
+serper_api_key = os.getenv("SERPER_API_KEY")
+
+#setting the environment variables
+os.environ["OPENAI_API_KEY"] = openai_api_key
+os.environ["SERPER_API_KEY"] = serper_api_key
+
+support_agent = Agent(
+    role="Senior Support Representative",
+	goal="Be the most friendly and helpful "
+        "support representative in your team",
+	backstory=(
+		"You work at crewAI (https://crewai.com) and "
+        " are now working on providing "
+		"support to {customer}, a super important customer "
+        " for your company."
+		"You need to make sure that you provide the best support!"
+		"Make sure to provide full complete answers, "
+        " and make no assumptions."
+	),
+	allow_delegation=False,
+    llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0.8),
+	verbose=True
+)
+
+support_quality_assurance_agent = Agent(
+	role="Support Quality Assurance Specialist",
+	goal="Get recognition for providing the "
+    "best support quality assurance in your team",
+	backstory=(
+		"You work at crewAI (https://crewai.com) and "
+        "are now working with your team "
+		"on a request from {customer} ensuring that "
+        "the support representative is "
+		"providing the best support possible.\n"
+		"You need to make sure that the support representative "
+        "is providing full"
+		"complete answers, and make no assumptions."
+	),
+    llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0.8),
+	verbose=True
+)
+
+from crewai_tools import SerperDevTool, \
+                         ScrapeWebsiteTool, \
+                         WebsiteSearchTool
+
+search_tool = SerperDevTool()
+scrape_tool = ScrapeWebsiteTool()
+
+#this means that the LLM will only scrape info off of that this CrewAI docs
+docs_scrape_tool = ScrapeWebsiteTool(
+    website_url="https://docs.crewai.com/how-to/Creating-a-Crew-and-kick-it-off/"
+)
+
+inquiry_resolution = Task(
+    description=(
+        "{customer} just reached out with a super important ask:\n"
+	    "{inquiry}\n\n"
+        "{person} from {customer} is the one that reached out. "
+		"Make sure to use everything you know "
+        "to provide the best support possible."
+		"You must strive to provide a complete "
+        "and accurate response to the customer's inquiry."
+    ),
+    expected_output=(
+	    "A detailed, informative response to the "
+        "customer's inquiry that addresses "
+        "all aspects of their question.\n"
+        "The response should include references "
+        "to everything you used to find the answer, "
+        "including external data or solutions. "
+        "Ensure the answer is complete, "
+		"leaving no questions unanswered, and maintain a helpful and friendly "
+		"tone throughout."
+    ),
+	tools=[docs_scrape_tool],
+    agent=support_agent,
+)
+
+quality_assurance_review = Task(
+    description=(
+        "Review the response drafted by the Senior Support Representative for {customer}'s inquiry. "
+        "Ensure that the answer is comprehensive, accurate, and adheres to the "
+		"high-quality standards expected for customer support.\n"
+        "Verify that all parts of the customer's inquiry "
+        "have been addressed "
+		"thoroughly, with a helpful and friendly tone.\n"
+        "Check for references and sources used to "
+        " find the information, "
+		"ensuring the response is well-supported and "
+        "leaves no questions unanswered."
+    ),
+    expected_output=(
+        "A final, detailed, and informative response "
+        "ready to be sent to the customer.\n"
+        "This response should fully address the "
+        "customer's inquiry, incorporating all "
+		"relevant feedback and improvements.\n"
+		"Don't be too formal, we are a chill and cool company "
+	    "but maintain a professional and friendly tone throughout."
+    ),
+    agent=support_quality_assurance_agent,
+)
+
+crew = Crew(
+  agents=[support_agent, support_quality_assurance_agent],
+  tasks=[inquiry_resolution, quality_assurance_review],
+  verbose=2,
+  memory=True
+)
+
+inputs = {
+    "customer": "DeepLearningAI",
+    "person": "Andrew Ng",
+    "inquiry": "I need help with setting up a Crew "
+               "and kicking it off, specifically "
+               "how can I add memory to my crew? "
+               "Can you provide guidance?"
+}
+
+result = crew.kickoff(inputs=inputs)
+
+from IPython.display import Markdown
+Markdown(result)
